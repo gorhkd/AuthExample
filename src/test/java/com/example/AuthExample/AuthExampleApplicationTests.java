0af9d75bd41d61197c3b18d2cf2244192d64407d
gorhkd <gorhkd223@gmail.com>
2025-03-25 09:41:11 +0900

    work

---
+++ src/test/java/com/example/AuthExample/AuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
